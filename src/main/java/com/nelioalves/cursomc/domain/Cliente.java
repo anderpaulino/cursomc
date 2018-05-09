47e143db4ea6ccef96a1fa79660ba72c7f668d20
Anderson Paulino <anderpaulino@gmail.com>
2018-05-09 17:08:27 -0400

    Usando padrão de nomes paraimagens

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -45,9 +45,7 @@ public class Cliente implements Serializable{
 	
 	@JsonIgnore
 	private String senha;
-	
-	private String imageUrl;
-	
+		
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
@@ -160,16 +158,6 @@ public class Cliente implements Serializable{
 
 	public void addPerfil(Perfil perfil) {
 		this.perfis.add(perfil.getCod());
-	}
-
-	
-	
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
 	}
 
 	@Override
